cd84a2d491418f2b5e5bb464e777e29375a9d0db
qtrinh2 <qtrinh2@gmu.edu>
2025-04-03 16:24:29 -0400

    test

--- django/sample/views.py
+++ django/sample/views.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
-from allauth.socialaccount.models import SocialApp
 
 def root(request):
-  print(SocialApp.objects.values())
   return render(request, 'tailwind.html')
